3e5eddb4834abbdf5e773364d15e7a9ad5fb63ad
JardonM <jardon.m.martins@gmail.com>
2021-11-28 16:54:57 -0300

    Criacao Repositorio e Resolucao [M1S04] - Ex1

---
+++ Semana 4/src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDateTime atual = LocalDateTime.now();
+
+        if (atual.getHour() > 5 && atual.getHour() < 13) {
+            System.out.println("Bom dia, no momento são " + atual.getHour() + ":" + atual.getMinute() + ".");
+        } else if (atual.getHour() >= 13 && atual.getHour() < 18) {
+            System.out.println("Boa tarde, no momento são " + atual.getHour() + ":" + atual.getMinute() + ".");
+        } else {
+            System.out.println("Boa noite, no momento são " + atual.getHour() + ":" + atual.getMinute() + ".");
+        }
+    }
+
+}
